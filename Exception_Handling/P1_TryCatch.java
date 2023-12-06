d6681d90c47dffa07cb3f5813143c8faedf8acca
manavdas1999 <dascorp009@gmail.com>
2023-12-06 08:32:53 +0530

    Update P1_TryCatch.java

--- Exception_Handling/P1_TryCatch.java
+++ Exception_Handling/P1_TryCatch.java
@@ -1,6 +1,6 @@
 package Exception_Handling;
 
-// // Level 1
+// // Level 1 - having an exception
 // public class P1_TryCatch{
 //     public static void main(String[] args) {
 //         int i,j,k;
@@ -16,26 +16,65 @@ package Exception_Handling;
 // }
 
 
-// // Level 2
+// // Level 2 - using try - catch to solve that error
+// public class P1_TryCatch{
+//     public static void main(String[] args) {
+//         int i,j,k;
+
+//         i = 8;
+//         j = 0;  
+
+//         k = 0;
+
+//         try {
+//         k = i/j;   // critical statement is wrapped with try block
+//         }
+//         // exception thrown by try is handled by catch
+//         // using object (e) of generic Exception class 
+//         catch(Exception e){
+//             System.out.println("Cannot be divided by zero");
+//         }
+        
+//         // k must be initialized, as if exception occurs k will stay uninitiaized
+//         System.out.println(k);   // this line is never reached
+//     }
+// }
+
+
+// // LEvel 3 - handling multiple exceptions
 public class P1_TryCatch{
     public static void main(String[] args) {
         int i,j,k;
+        int[] arr = new int[4];
 
         i = 8;
-        j = 0;  
-
+        j = 1;  
         k = 0;
 
+        // now we have multiple critical statments each which may throw different types of exceptions
         try {
-        k = i/j;   // critical statement is wrapped with try block
+        k = i/j;   // ArithmeticException
+
+        // ArrayOutOfBoundsException
+        for(int in = 0; in<=4; in++){
+            arr[in] = 1;
         }
-        // exception thrown by try is handled by catch
-        // using object (e) of generic Exception class 
-        catch(Exception e){
+        }
+        catch(ArithmeticException ae){
             System.out.println("Cannot be divided by zero");
         }
+        catch(ArrayIndexOutOfBoundsException arre){
+            System.out.println("Cannot access this index of array");
+        }
+        // this can handle any other type of errors
+        catch(Exception e){
+            System.out.println();
+        }
         
-        // k must be initialized, as if exception occurs k will stay uninitiaized
-        System.out.println(k);   // this line is never reached
+        
+        System.out.println(k); 
+        for(int val : arr){
+            System.out.print(val + ", ");
+        };
     }
 }
